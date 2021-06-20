9364e0fe0fb1178acbf70b06e3f94e478c77f050
choi sunmin <1csm9898@naver.com>
2021-06-20 17:11:10 +0900

    ver6

--- lionproject/settings.py
+++ lionproject/settings.py
@@ -143,5 +143,5 @@ AWS_S3_SIGNATURE_VERSION='s3v4'
 AWS_S3_REGION_NAME='ap-northeast-2'
 
 import dj_database_url
-db_from_env=dj_database_url.config(coon_max_age=500)
+db_from_env=dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
